5b5f1bf106cc85ff6494c43b99dd8b7f1cb89335
Меркулов Александр Андреевич [012739] <merkulovwork@mail.ru>
2024-06-01 10:32:52 +1000

    добавил ошибку загрузки файла, добавил кнопку сохранения файлов в пдф, добавил стилевую красоту

--- medapp/urls.py
+++ medapp/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from medapp.views import main_page, upload_file
+from medapp.views import main_page, upload_file, instruction, save_result
 
 from medmed import settings
 
 urlpatterns = [
     path('', main_page),
-    path('form', upload_file)
+    path('form', upload_file),
+    path('instruction', instruction),
+    path('save_result/', save_result),
 ]
 
 if settings.DEBUG:
--- medapp/views.py
+++ medapp/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponseRedirect
+import io
+
+from django.http import HttpResponseRedirect, FileResponse
 from django.shortcuts import render
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, TableStyle, Table
+
 from .forms import UploadForm
 import os
 
@@ -12,6 +22,14 @@ def main_page(request):
     return render(request, 'index.html')
 
 
+def instruction(request):
+    return render(request, 'instruction.html')
+
+def checkFile(filepath):
+    print(filepath)
+
+
+
 def file_handler(file):
     UPLOAD_DIRECTORY = settings.MEDIA_ROOT
     if not os.path.exists(UPLOAD_DIRECTORY):
@@ -22,14 +40,82 @@ def file_handler(file):
     with open(filepath, 'wb+') as d:
         for ch in file.chunks():
             d.write(ch)
+
+    # здесь должна быть функция, которой я передам управление файлом,
+    # допустим для его чтения - отдаём filepath.
+
     pass
 
 
 def upload_file(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
-        file_handler(request.FILES['file'])
-        return HttpResponseRedirect('/')
+        if form.is_valid():
+            file_handler(request.FILES['file'])
+
+
+            apnoe = None
+            apnoe_type = None
+            context = {
+                'apnoe': apnoe,
+                'apnoe_type': apnoe_type,
+                'filename': request.FILES['file'].name
+            }
+
+            return render(request, 'result.html', context)
+            # return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/err_load_file')
     else:
         form = UploadForm()
         return render(request, 'form.html', {'form':form})
+
+
+def save_result(request):
+
+    # получаем параметры из запроса на сохранение файла
+    params = request.GET.items()
+
+    pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
+
+    # Настройка PDF
+    pdf_filename = "result.pdf"
+    buffer = io.BytesIO()
+    doc = SimpleDocTemplate(buffer, pagesize=letter)
+    elements = []
+
+    # Определение стилей
+    styles = getSampleStyleSheet()
+    styleH = styles["Heading1"]
+
+    # Форматирование данных в виде списка списков для таблицы
+    table_header = []
+    # table_header = ['Апное', "Тип апное", "Название файла"]
+    table_body = []
+    for param in params:
+        table_header.append(param[0])
+        table_body.append(param[1])
+    table_data = [table_header, table_body]  # заголовки таблицы
+
+    # Создание таблицы с данными
+    table = Table(table_data, repeatRows=1)
+    table.setStyle(TableStyle([
+                               ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+                               ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                               ('FONTNAME', (0, 0), (-1, -1), 'Arial'),
+                               ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                               ]))
+
+    # Добавление таблицы в элементы PDF
+    elements.append(table)
+
+    # Генерация PDF
+    doc.build(elements)
+
+    # Сброс указателя потока обратно в начало
+    buffer.seek(0)
+
+    # Создание FileResponse
+    response = FileResponse(buffer, as_attachment=True, filename=pdf_filename)
+
+    return response
